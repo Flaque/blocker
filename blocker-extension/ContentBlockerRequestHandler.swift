881a8717ee740e998711b3a6d4a14923019e705f
Flaqueeau <econrad@zagmail.gonzaga.edu>
2018-03-20 11:02:25 -0700

    Init Commit

---
+++ blocker-extension/ContentBlockerRequestHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ContentBlockerRequestHandler.swift
+//  blocker-extension
+//
+//  Created by Evan Conrad on 3/20/18.
+//  Copyright © 2018 Evan Conrad. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequest(with context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json"))!
+        
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+        
+        context.completeRequest(returningItems: [item], completionHandler: nil)
+    }
+    
+}
